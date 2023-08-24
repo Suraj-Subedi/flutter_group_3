aa9617b08dbea25e01e25f289553d00d6d2844c7
Suraj <soorajsubedi1227@gmail.com>
2023-08-24 15:52:59 +0545

    basic containers

--- lib/main.dart
+++ lib/main.dart
@@ -9,76 +9,146 @@ void main() {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Center(
-            child: Text(
-              'The count is :',
-              style: TextStyle(
-                fontSize: 50,
+      body: SafeArea(
+        child: Container(
+          color: Colors.grey,
+          width: double.maxFinite,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.red,
+                height: 100,
+                width: 100,
+              ),
+              Container(
                 color: Colors.blue,
+                height: 100,
+                width: 100,
               ),
-            ),
-          ),
-          Center(
-            child: Text(
-              count.toString(),
-              style: TextStyle(
-                fontSize: 50,
+              Container(
+                color: Colors.yellow,
+                height: 100,
+                width: 100,
               ),
-            ),
+              Container(
+                color: Colors.green,
+                height: 100,
+                width: 100,
+              ),
+            ],
           ),
-          // TextButton(
-          //   onPressed: () {
-          //     print(count);
-
-          //     setState(() {
-          //       count++;
-          //     });
-
-          //     print(count);
-          //   },
-          //   child: Text(
-          //     'Click',
-          //     style: TextStyle(
-          //       fontSize: 50,
-          //       color: Colors.red,
-          //     ),
-          //   ),
-          // )
-        ],
-      ),
-      appBar: AppBar(
-        leading: const Icon(Icons.menu),
-        title: const Text('My App'),
-        centerTitle: true,
-        actions: const [
-          Icon(Icons.search),
-          Icon(Icons.more_vert),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            count++;
-          });
-        },
+        ),
       ),
     );
   }
 }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   var count = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Container(
+//           decoration: BoxDecoration(
+//             borderRadius: BorderRadius.circular(20),
+//             color: Colors.blue,
+//             // shape:BoxShape.circle
+//           ),
+//           child: Container(
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(100),
+//               color: Colors.red,
+//               // shape:BoxShape.circle
+//             ),
+//             padding: EdgeInsets.all(20),
+//             margin: EdgeInsets.all(20),
+//             child: Container(
+//               color: Colors.black,
+//               child: Text(
+//                 'My Container',
+//                 style: TextStyle(
+//                   fontSize: 40,
+//                   color: Colors.white,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//       // body: Column(
+//       //   mainAxisAlignment: MainAxisAlignment.center,
+//       //   children: [
+//       //     const Center(
+//       //       child: Text(
+//       //         'The count is :',
+//       //         style: TextStyle(
+//       //           fontSize: 50,
+//       //           color: Colors.blue,
+//       //         ),
+//       //       ),
+//       //     ),
+//       //     Center(
+//       //       child: Text(
+//       //         count.toString(),
+//       //         style: TextStyle(
+//       //           fontSize: 50,
+//       //         ),
+//       //       ),
+//       //     ),
+//       // TextButton(
+//       //   onPressed: () {
+//       //     print(count);
+
+//       //     setState(() {
+//       //       count++;
+//       //     });
+
+//       //     print(count);
+//       //   },
+//       //   child: Text(
+//       //     'Click',
+//       //     style: TextStyle(
+//       //       fontSize: 50,
+//       //       color: Colors.red,
+//       //     ),
+//       //   ),
+//       // )
+//       //   ],
+//       // ),
+//       appBar: AppBar(
+//         leading: const Icon(Icons.menu),
+//         title: const Text('My App'),
+//         centerTitle: true,
+//         actions: const [
+//           Icon(Icons.search),
+//           Icon(Icons.more_vert),
+//         ],
+//       ),
+//       // floatingActionButton: FloatingActionButton(
+//       //   child: Icon(Icons.add),
+//       //   onPressed: () {
+//       //     setState(() {
+//       //       count++;
+//       //     });
+//       //   },
+//       // ),
+//     );
+//   }
+// }
