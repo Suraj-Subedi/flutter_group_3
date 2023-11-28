15b4b477bee68e316192744daa688509bdf0c05e
Suraj Subedi <soorajsubedi1227@gmail.com>
2023-11-28 13:06:52 +0545

    get specializations api integrated

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,10 +1,16 @@
+import 'package:ecom_3/app/models/specalization.dart';
+import 'package:ecom_3/app/utils/constants.dart';
+import 'package:ecom_3/app/utils/memory.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class HomeController extends GetxController {
-  var count = 0.obs;
+  SpecializationResponse? specializationResponse;
+
   @override
   void onInit() {
     super.onInit();
+    getSpecializations();
   }
 
   @override
@@ -17,7 +23,29 @@ class HomeController extends GetxController {
     super.onClose();
   }
 
-  void increaseCount() {
-    count.value++;
+  void getSpecializations() async {
+    try {
+      var url = Uri.http(ipAddress, 'doctor_api/getSpecialization');
+
+      var response = await http.post(url, body: {"token": Memory.getToken()});
+      specializationResponse = specializationResponseFromJson(response.body);
+      update();
+
+      if (specializationResponse?.success ?? false) {
+        // showCustomSnackBar(
+        //   message: specializationResponse?.message ?? '',
+        //   isSuccess: true,
+        // );
+      } else {
+        showCustomSnackBar(
+          message: specializationResponse?.message ?? '',
+        );
+      }
+    } catch (e) {
+      print(e);
+      showCustomSnackBar(
+        message: 'Something went wrong',
+      );
+    }
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -8,11 +8,65 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    Get.lazyPut(() => HomeController());
+    Get.put(HomeController());
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+        title: const Text('Mero Doctor'),
+      ),
+      body: GetBuilder<HomeController>(
+        builder: (controller) {
+          if (controller.specializationResponse == null) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Specializations',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: controller
+                            .specializationResponse?.specalizations?.length ??
+                        0,
+                    itemBuilder: (context, index) => Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.blue.withOpacity(0.7),
+                      ),
+                      margin: const EdgeInsets.only(
+                        right: 10,
+                        top: 10,
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Center(
+                        child: Text(
+                          controller.specializationResponse
+                                  ?.specalizations?[index].title ??
+                              '',
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
