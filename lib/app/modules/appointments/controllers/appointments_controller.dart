45b15fbefdd04a177f8a43bff6c07760a27b62a6
Suraj Subedi <soorajsubedi1227@gmail.com>
2023-12-07 13:17:18 +0545

    showing appointments

--- lib/app/modules/appointments/controllers/appointments_controller.dart
+++ lib/app/modules/appointments/controllers/appointments_controller.dart
@@ -1,23 +1,40 @@
+import 'package:ecom_3/app/models/appointment.dart';
+import 'package:ecom_3/app/utils/constants.dart';
+import 'package:ecom_3/app/utils/memory.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class AppointmentsController extends GetxController {
-  //TODO: Implement AppointmentsController
+  AppointmentResponse? appointmentResponse;
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    getAppointments();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  void getAppointments() async {
+    try {
+      Uri url = Uri.http(ipAddress, 'doctor_api/getAppointments');
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+      var response = await http.post(url, body: {"token": Memory.getToken()});
 
-  void increment() => count.value++;
+      // print(response.body);
+
+      var result = appointmentResponseFromJson(response.body);
+
+      if (result.success ?? false) {
+        appointmentResponse = result;
+        update();
+      } else {
+        showCustomSnackBar(
+          message: result.message ?? '',
+        );
+      }
+    } catch (e) {
+      showCustomSnackBar(
+        message: e.toString(),
+      );
+    }
+  }
 }
